4c774f89bd43aa29b37d651c01d81dbc942b97ed
yuma140902 <yuma140902@gmail.com>
2021-11-05 23:58:49 +0900

    typesモジュールを追加 (ReverieEngine)

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,6 @@
-pub use nalgebra as na;
 pub use reverie_engine as re;
+
+pub use re::types::*;
 
 use crate::{TEX_ATLAS_H, TEX_ATLAS_W, TEX_H, TEX_W};
 
@@ -8,10 +9,6 @@ pub type CuboidTextures<'a> =
     re::vao::vao_builder::CuboidTextures<'a, TEX_W, TEX_H, TEX_ATLAS_W, TEX_ATLAS_H>;
 pub type VaoBuilder<'a> =
     re::vao::vao_builder::VaoBuilder<'a, TEX_W, TEX_H, TEX_ATLAS_W, TEX_ATLAS_H>;
-
-pub type Vector3 = na::Vector3<f32>;
-pub type Matrix4 = na::Matrix4<f32>;
-pub type Point3 = na::Point3<f32>;
 
 // prelude
 pub use crate::socketio_encoding::ToUtf8String;
