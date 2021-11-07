d5bbd0f5c79b3c57f4e699aebc0dc3a1e670459e
yuma140902 <yuma140902@gmail.com>
2021-11-07 17:52:20 +0900

    add diff_{up, down, left, right}

--- src/field.rs
+++ src/field.rs
@@ -22,8 +22,8 @@ impl Field<FIELD_SIZE, FIELD_SIZE> {
     }
 
     pub fn update(&mut self, height_map: na::SMatrix<i32, FIELD_SIZE, FIELD_SIZE>) {
-        self.renderer.make_diff(&self.map, &height_map);
         self.map = height_map;
+        self.renderer.make_diff(&self.map);
     }
 
     pub fn render(&mut self) -> &VaoBuffer {
@@ -34,13 +34,35 @@ impl Field<FIELD_SIZE, FIELD_SIZE> {
 
 struct FieldRenderer<const X: usize, const Z: usize> {
     vao_buffer: VaoBuffer,
+    diff_up: na::SMatrix<i32, X, Z>,
+    diff_down: na::SMatrix<i32, X, Z>,
+    diff_left: na::SMatrix<i32, X, Z>,
+    diff_right: na::SMatrix<i32, X, Z>,
 }
 
 impl FieldRenderer<FIELD_SIZE, FIELD_SIZE> {
     pub fn new() -> Self {
         let mut vao_buffer = VaoBuffer::with_num_vertex(36 * FIELD_SIZE * FIELD_SIZE); // 立方体は36頂点から成る
         vao_buffer.add_floor(FIELD_SIZE, FIELD_SIZE);
-        Self { vao_buffer }
+        Self {
+            vao_buffer,
+            diff_up: na::SMatrix::<i32, FIELD_SIZE, FIELD_SIZE>::zeros(),
+            diff_down: na::SMatrix::<i32, FIELD_SIZE, FIELD_SIZE>::zeros(),
+            diff_left: na::SMatrix::<i32, FIELD_SIZE, FIELD_SIZE>::zeros(),
+            diff_right: na::SMatrix::<i32, FIELD_SIZE, FIELD_SIZE>::zeros(),
+        }
+    }
+
+    pub fn make_diff(&mut self, map: &na::SMatrix<i32, FIELD_SIZE, FIELD_SIZE>) {
+        let slide_upward = map.clone().remove_row(0).insert_row(FIELD_SIZE - 2, 0);
+        let slide_downward = map.clone().remove_row(FIELD_SIZE - 1).insert_row(0, 0);
+        let slide_left = map.clone().remove_column(0).insert_column(FIELD_SIZE - 2, 0);
+        let slide_right = map.clone().remove_column(FIELD_SIZE - 1).insert_column(0, 0);
+
+        self.diff_up = map - slide_upward;
+        self.diff_down = map - slide_downward;
+        self.diff_left = map - slide_right; // ←  slide_leftと間違えているわけではない。1つ右にスライドした行列との差を取ると、各要素について1つ左の要素との差が取れる
+        self.diff_right = map - slide_left;
     }
 
     pub fn render(&mut self, map: &na::SMatrix<i32, FIELD_SIZE, FIELD_SIZE>) {
