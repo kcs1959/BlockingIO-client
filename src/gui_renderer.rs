b314fade28de60820ef479b4795c66b0b8cf1514
yuma140902 <yuma140902@gmail.com>
2021-11-17 19:44:47 +0900

    勝敗の理由を表示

--- src/gui_renderer.rs
+++ src/gui_renderer.rs
@@ -14,6 +14,11 @@ const TEX_負け: Rect<i32, u32> = Rect::new_const(0, 24, 64, 12);
 const TEX_待機中: Rect<i32, u32> = Rect::new_const(0, 36, 52, 12);
 const TEX_接続中: Rect<i32, u32> = Rect::new_const(0, 48, 36, 12);
 const TEX_引き分け: Rect<i32, u32> = Rect::new_const(0, 72, 64, 12);
+const TEX_相手が落下しました: Rect<i32, u32> = Rect::new_const(0, 84, 8 * 9, 12);
+const TEX_相手がつかまりました: Rect<i32, u32> = Rect::new_const(0, 96, 8 * 10, 12);
+const TEX_落下してしまった: Rect<i32, u32> = Rect::new_const(0, 108, 8 * 8, 12);
+const TEX_つかまってしまった: Rect<i32, u32> = Rect::new_const(0, 120, 8 * 9, 12);
+const TEX_異常終了: Rect<i32, u32> = Rect::new_const(0, 132, 8 * 4, 12);
 
 pub struct GuiRenderer {
     buffer: VaoBuffer,
@@ -26,6 +31,11 @@ pub struct GuiRenderer {
     tex_待機中: DynamicTextureUV,
     tex_接続中: DynamicTextureUV,
     tex_引き分け: DynamicTextureUV,
+    tex_相手が落下しました: DynamicTextureUV,
+    tex_相手がつかまりました: DynamicTextureUV,
+    tex_落下してしまった: DynamicTextureUV,
+    tex_つかまってしまった: DynamicTextureUV,
+    tex_異常終了: DynamicTextureUV,
 }
 
 impl GuiRenderer {
@@ -39,6 +49,15 @@ impl GuiRenderer {
         let tex_待機中 = DynamicTextureUV::new(&TEX_待機中, gui_texture.width, gui_texture.height);
         let tex_接続中 = DynamicTextureUV::new(&TEX_接続中, gui_texture.width, gui_texture.height);
         let tex_引き分け = DynamicTextureUV::new(&TEX_引き分け, gui_texture.width, gui_texture.height);
+        let tex_相手が落下しました =
+            DynamicTextureUV::new(&TEX_相手が落下しました, gui_texture.width, gui_texture.height);
+        let tex_相手がつかまりました =
+            DynamicTextureUV::new(&TEX_相手がつかまりました, gui_texture.width, gui_texture.height);
+        let tex_落下してしまった =
+            DynamicTextureUV::new(&TEX_落下してしまった, gui_texture.width, gui_texture.height);
+        let tex_つかまってしまった =
+            DynamicTextureUV::new(&TEX_つかまってしまった, gui_texture.width, gui_texture.height);
+        let tex_異常終了 = DynamicTextureUV::new(&TEX_異常終了, gui_texture.width, gui_texture.height);
 
         Self {
             buffer: VaoBuffer::new(),
@@ -51,6 +70,11 @@ impl GuiRenderer {
             tex_待機中,
             tex_接続中,
             tex_引き分け,
+            tex_相手が落下しました,
+            tex_相手がつかまりました,
+            tex_落下してしまった,
+            tex_つかまってしまった,
+            tex_異常終了,
         }
     }
 
@@ -110,6 +134,25 @@ impl GuiRenderer {
         );
     }
 
+    fn draw_sub_text(
+        buffer: &mut VaoBuffer,
+        tex: &DynamicTextureUV,
+        tex_rect: &Rect<i32, u32>,
+        window_width: u32,
+        window_height: u32,
+    ) {
+        buffer.add_layout_rectangle(
+            tex,
+            window_width,
+            window_height,
+            &Origin::Bottom,
+            &Position::Center(0),
+            &Position::Center(-30),
+            tex_rect.width() * 2,
+            tex_rect.height() * 2,
+        );
+    }
+
     pub fn draw_接続中(&mut self) {
         Self::draw_text(
             &mut self.buffer,
@@ -150,6 +193,16 @@ impl GuiRenderer {
         );
     }
 
+    pub fn draw_異常終了(&mut self) {
+        Self::draw_text(
+            &mut self.buffer,
+            &self.tex_異常終了,
+            &TEX_異常終了,
+            self.window_width,
+            self.window_height,
+        );
+    }
+
     pub fn draw_引き分け(&mut self) {
         Self::draw_text(
             &mut self.buffer,
@@ -158,6 +211,46 @@ impl GuiRenderer {
             self.window_width,
             self.window_height,
         );
+    }
+
+    pub fn draw_相手が落下しました(&mut self) {
+        Self::draw_sub_text(
+            &mut self.buffer,
+            &self.tex_相手が落下しました,
+            &TEX_相手が落下しました,
+            self.window_width,
+            self.window_height,
+        )
+    }
+
+    pub fn draw_相手がつかまりました(&mut self) {
+        Self::draw_sub_text(
+            &mut self.buffer,
+            &self.tex_相手がつかまりました,
+            &TEX_相手がつかまりました,
+            self.window_width,
+            self.window_height,
+        )
+    }
+
+    pub fn draw_落下してしまった(&mut self) {
+        Self::draw_sub_text(
+            &mut self.buffer,
+            &self.tex_落下してしまった,
+            &TEX_落下してしまった,
+            self.window_width,
+            self.window_height,
+        )
+    }
+
+    pub fn draw_つかまってしまった(&mut self) {
+        Self::draw_sub_text(
+            &mut self.buffer,
+            &self.tex_つかまってしまった,
+            &TEX_つかまってしまった,
+            self.window_width,
+            self.window_height,
+        )
     }
 
     pub fn render(&self, gl: &Gl, vao_config: &VaoConfig) {
